25f44c205affbb2479ef8143a1c8ac94a42b2d3c
NJORDRE001 <njordre001@vplincsci088.ad.csbsju.edu>
2019-03-26 22:17:13 -0500

    test classes

---
+++ tests/cmc/entity/SavedSchoolTest.java
@@ -0,0 +1,34 @@
+package cmc.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SavedSchoolTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSavedSchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+}
