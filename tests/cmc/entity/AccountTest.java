abcd5e1e36df2772f13b2f0f87f25bf580d212d9
LHOESCHEN001 <lhoeschen001@vplincsci018.ad.csbsju.edu>
2019-04-01 23:59:00 -0500

    lindsey

--- tests/cmc/entity/AccountTest.java
+++ tests/cmc/entity/AccountTest.java
@@ -7,64 +7,75 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
-
+	Account a;
+	
 	@Before
 	public void setUp() throws Exception {
+		a = new Account("Dummy", "Jordre", "DummyUser", "Password", 'u', 'Y');
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		a = null;
 	}
 
 	@Test
 	public void testSetFirstName() {
-		fail("Not yet implemented");
+		assertTrue("Name should be unchanged", a.getFirstName().equals("Dummy"));
+		a.setFirstName("Nate");
+		assertTrue("Name should be changed", a.getFirstName().equals("Nate"));
 	}
 
 
 	@Test
 	public void testSetLastName() {
-		fail("Not yet implemented");
+		assertTrue("Name should be unchanged", a.getLastName().equals("Jordre"));
+		a.setLastName("Dummy");
+		assertTrue("Name should be changed", a.getLastName().equals("Dummy"));
 	}
 
 	@Test
 	public void testSetPassword() {
-		fail("Not yet implemented");
+		assertTrue("Password should be unchanged", a.getPassword().equals("Password"));
+		a.setPassword("Pass");
+		assertTrue("Password should be changed", a.getPassword().equals("Pass"));
 	}
 
 	@Test
 	public void testSetUserType() {
-		fail("Not yet implemented");
+		assertTrue("Type should be unchanged", a.getUserType() == 'u');
+		a.setUserType('a');
+		assertTrue("Type should be changed", a.getUserType() == 'a');
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
+		assertTrue("Status should be unchanged", a.getStatus() == 'Y');
+		a.setStatus('N');
+		assertTrue("Status should be changed", a.getStatus() == 'N');
 	}
 
 	@Test
 	public void testSetUsername() {
-		fail("Not yet implemented");
+		assertTrue("Username should be unchanged", a.getUsername().equals("DummyUser"));
+		a.setUsername("NateTheDummy");
+		assertTrue("Username should be changed", a.getUsername().equals("NateTheDummy"));
 	}
 
 	@Test
-	public void testLogOn() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLogOff() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsLoggedOn() {
-		fail("Not yet implemented");
+	public void testLogOnAndLogOffAndIsLoggedOff() {
+		assertFalse("Account should be logged off", a.isLoggedOn());
+		a.logOn();
+		assertTrue("Account should be logged off", a.isLoggedOn());
+		a.logOff();
+		assertFalse("Account should be logged off", a.isLoggedOn());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String expected = "firstName= Dummy\nlastName= Jordre\nusername= DummyUser\npassword= "
+				+ "Password\nuserType= u\nstatus= Y\nisLoggedOn= false";
+		assertTrue("ToString should be " + expected, a.toString().equals(expected));
 	}
 
 }
